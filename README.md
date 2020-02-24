becee6e7a729656a2718a8375f8bf7db2eaf82cc
AndrijaMa <60138623+AndrijaMa@users.noreply.github.com>
2020-02-24 10:22:41 +0100

    New file
    Adding tableau server registration file

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# test
+# Automated installation of Tableau Server on Microsoft Azure
+# The goal is to:
+    - Setup a Server in Azure 
+    - Download Tableau Server media
+    - Install and Configure Tableau Server
--- download.ps1
+++ download.ps1
@@ -1,10 +1,12 @@
 $version_major = "2019"
 $version_minor = "3"
 $version_x = "1"
+$folder = "C:\Downloads\"
 $url = "https://downloads.tableau.com/esdalt/"+$version_major+"."+$version_minor+"."+$version_x+"/TableauServer-64bit-"+$version_major+"-"+$version_minor+"-"+$version_x+".exe"
-$output = "$PSScriptRoot\tableau_server.exe"
+$output = $Folder+"tableau_server.exe"
 $start_time = Get-Date
 
+New-Item -Path $Folder -ItemType Directory
 #Download file
 Invoke-WebRequest -Uri $url -OutFile $output
 Write-Output "Time taken: $((Get-Date).Subtract($start_time).Seconds) second(s)"
