e59a4db057b25d6eec0634a2b1f96984ef2d20c9
AndrijaMa <60138623+AndrijaMa@users.noreply.github.com>
2020-02-27 14:37:30 +0100

    Update download.ps1
    Added robust error handling for $Version input

--- download.ps1
+++ download.ps1
@@ -1,31 +1,65 @@
 param   (
             [Parameter(Mandatory=$true)]  [String]$LicenseKey, 
             [Parameter(Mandatory=$false)] [String]$Version,
+            [Parameter(Mandatory=$false)] [String]$Bootstrap=$false,
             [Parameter(Mandatory=$false)] [String]$Help 
-        )
+)
 
-$Help = Write-Host "-License: Enter you tableau server key or type Trial if you want to use a 14 day trial"
+function func_Version ($Version) {
+   
+    if(!$Version)
+    {
+        Write-Host "-Version is missing a value. It should be in the format xxxx.x.x like for example 2019.1.4 or type Trial to active a 14 day trial"
+    }
+    elseif($version.ToString().Length -ne 8)
+    {
+        Write-Host "-Version is in the wrong format. It should be in the format xxxx.x.x like for example 2019.1.4"
+        
+    }
 
-$github_url = "https://raw.githubusercontent.com/AndrijaMa/Test/master/"
-$folder = "C:\Downloads\"
-$reg_file = "reginfo.json"
-$iDP_config = "iDP_config.json"
-$log_file = "install.log"
-$event_file = "event.log"
-$version_major = "2019"
-$version_minor = "3"
-$version_hotfix = "1"
+    elseif($version.ToString().Length -eq 8)
+            {
+            if ($version -like '*.*')
+            {
+                $major = $version.substring(0,4)
+                $minor = $version.substring(0,$version.lastindexof('.')).substring(5)
+                $hotfix = $version.substring($version.length-1)
+                
+            }
+            elseif ($version -like '*-*')
+            {
+                $version = $version.ToString().replace('-','.') 
+                $major = $version.substring(0,4)
+                $minor = $version.substring(0,$version.lastindexof('.')).substring(5)
+                $hotfix = $version.substring($version.length-1)
+                
+            }
+        }
+        return $major, $minor, $hotfix
+}
 
-                            
+function func_Variables()
+                            {
+                                $github_url = "https://raw.githubusercontent.com/AndrijaMa/Test/master/"
+                                $folder = "C:\Downloads\"
+                                $reg_file = "reginfo.json"
+                                $iDP_config = "iDP_config.json"
+                                $log_file = "install.log"
+                                $event_file = "event.log"
+                                $bootstrapfile = "bootstrapfile.json"
+                                return $github_url, $folder, $reg_file, $iDP_config, $log_file, $event_file, $bootstrapfile                           
+}
+func_Variables
+
 function Write-ToLog ($text) {
     
     $message = "[{0:yyyy/MM/dd/} {0:HH:mm:ss}]" -f (Get-Date) +", "+ $text 
     Write-Host  $message
-    Write-Output $message | Out-file $folder$log_file -Append -Encoding default
+    Write-Output $message | Out-file $folder$event_file -Append -Encoding default
 
 }
 
-function install(
+function func_Install(
                     $github_url, 
                     $folder, 
                     $reg_file, 
@@ -50,9 +84,9 @@ function install(
                                     }
 
                                 #Download the server installation file
-                                Write-ToLog -text "Started downloading Tableau Server media" 
+                                Write-ToLog -text "Starting Tableau Server installation media download..." 
                                 Invoke-WebRequest -Uri $url -OutFile $folder"tableau_server.exe"
-                                Write-ToLog -text "Finnished downloading Tableau Server media"
+                                Write-ToLog -text "Finnished Tableau Server installation media download"
 
                                 #Download reg_file
                                 Write-ToLog -text  "Downloading reg file"
@@ -60,30 +94,31 @@ function install(
                                 Write-ToLog -text "Download of regfile completed successfully"
                                 
                                 #Download iDP file
-                                Write-ToLog -text  "Downloading reg file"
-                                Invoke-WebRequest -Uri $github_url + $iDP_config -OutFile $folder$iDP_config
-                                Write-ToLog -text "Download of regfile completed successfully"
+                                Write-ToLog -text  "Downloading iDP config file"
+                                Invoke-WebRequest -Uri $github_url$iDP_config -OutFile $folder$iDP_config
+                                Write-ToLog -text "Download of iDP config file completed successfully"
 
                                 #Install Switches and Properties for Tableau Server
                                 #https://help.tableau.com/current/server/en-us/silent_installer_flags.htm
                                 #Start silent Tableau server installation
                                 Write-ToLog -text  "Starting Tableau Server installation"
-                                Start-Process -FilePath $folder"tableau_server.exe" -ArgumentList "'/install /silent /ACCEPTEULA = 1 /LOG '+$folder$log_file+'" -Verb RunAs -Wait
-                                Write-ToLog -text  "Tableau server installation finnished"
+                                Start-Process -FilePath $folder"tableau_server.exe" -ArgumentList "/install /silent /ACCEPTEULA = 1 /LOG '$folder$log_file'" -Verb RunAs -Wait
+                                Write-ToLog -text  "Tableau Server installation completed successfully"
 
-
-                                #Add TSM to Windows Path
+                                #Identifying path to TSM
+                                #Get-ItemProperty -Path HKLM:\SOFTWARE\Tableau $version_full*
                                 Write-ToLog -text "Adding TSM to local Windows system PATH"
                                 $wd =  'C:\Program Files\Tableau\Tableau Server\packages\'
                                 $tsm_path = Get-ChildItem $wd\bin.$version_major$version_minor.$version_hotfix* -Directory | Select-Object -Property name  
                                 $tsm_path = $wd+$tsm_path.Name
-                                
+                                #Add TSM to Windows Path
                                 $Env:path += $tsm_path
-                                #Activate license
-                                Write-Host "Activating Tableau Server License"
+
+                                #Activate Tableau Server license
+                                Write-ToLog -text  "Registering Tableau Server License"
                                 if($LicenseKey -eq '')
                                 {
-                                    Write-Host "-LicenseKey parameter was not entered       ! Please enter your license key or type Trial to activeate a 14 day trial key."
+                                    Write-Host "-LicenseKey was empty! Please enter your license key or type Trial to activeate a 14 day trial key."
                                 }
                                 elseif($LicenseKey.ToLower() -eq "trial")
                                 {
@@ -99,7 +134,7 @@ function install(
                                 }
                                 
                                 #Register Tableau Server
-                                Write-ToLog -text  "Starting Tableau Server registration"
+                                Write-ToLog -text "Starting Tableau Server registration"
                                 Invoke-Expression "tsm register --file '$Folder$reg_file'"
                                 Write-ToLog -text "Completed Tableau Server registration"
 
@@ -111,22 +146,29 @@ function install(
                                 #Apply pending changes
                                 Write-ToLog -text "Applying pending TSM changes"
                                 Invoke-Expression "tsm pending-changes apply"
-                                Write-ToLog -text  "Completed TM changes"
+                                Write-ToLog -text "TSM changes applied successfully."
 
                                 #Initialize configuration
-                                Write-ToLog -text  "Initializing Tableau Server"
+                                Write-ToLog -text "Initializing Tableau Server"
                                 Invoke-Expression "tsm initialize"
-                                Write-ToLog -text  "Tableau Server initialized"
+                                Write-ToLog -text "Tableau Server initialized"
 
                                 #Initialize configuration
-                                Write-ToLog -text  "Starting Tableau Server"
+                                Write-ToLog -text "Starting Tableau Server"
                                 Invoke-Expression "tsm start"
-                                Write-ToLog -text  "Tableau Server started"
+                                Write-ToLog -text "Tableau Server started"
+
+                                #Generate bootstrap file
+                                if($Bootstrap -eq $true)
+                                {
+                                    Invoke-Expression "tsm topology nodes get-bootstrap-file --file '$folder$bootstrapfile'"
+                                }
                         }
                         catch
                             {
                                 Write-ToLog -text $PSItem.Exception.Message
                             }
 }
-
-
+func_Variables
+func_Version -Version $Version
+func_Install -github_url $github_url -folder $folder -reg_file $reg_file -iDP_config $iDP_config -log_file $log_file -event_file $event_file -version_major $major -version_minor $minor -version_hotfix $hotfix
