59f163da59363b4f4e520fb1a380e3214dcc3c11
AndrijaMa <60138623+AndrijaMa@users.noreply.github.com>
2020-02-27 09:33:40 +0100

    New functionality
    Added functions and error handling

--- download.ps1
+++ download.ps1
@@ -1,14 +1,132 @@
+param   (
+            [Parameter(Mandatory=$true)]  [String]$LicenseKey, 
+            [Parameter(Mandatory=$false)] [String]$Version,
+            [Parameter(Mandatory=$false)] [String]$Help 
+        )
+
+$Help = Write-Host "-License: Enter you tableau server key or type Trial if you want to use a 14 day trial"
+
+$github_url = "https://raw.githubusercontent.com/AndrijaMa/Test/master/"
+$folder = "C:\Downloads\"
+$reg_file = "reginfo.json"
+$iDP_config = "iDP_config.json"
+$log_file = "install.log"
+$event_file = "event.log"
 $version_major = "2019"
 $version_minor = "3"
-$version_x = "1"
-$folder = "C:\Downloads\"
-$url = "https://downloads.tableau.com/esdalt/"+$version_major+"."+$version_minor+"."+$version_x+"/TableauServer-64bit-"+$version_major+"-"+$version_minor+"-"+$version_x+".exe"
-$output = $Folder+"tableau_server.exe"
-$start_time = Get-Date
+$version_hotfix = "1"
 
-New-Item -Path $Folder -ItemType Directory
-#Download file
-Invoke-WebRequest -Uri $url -OutFile $output
-Write-Output "Time taken: $((Get-Date).Subtract($start_time).Seconds) second(s)"
+                            
+function Write-ToLog ($text) {
+    
+    $message = "[{0:yyyy/MM/dd/} {0:HH:mm:ss}]" -f (Get-Date) +", "+ $text 
+    Write-Host  $message
+    Write-Output $message | Out-file $folder$log_file -Append -Encoding default
+
+}
+
+function install(
+                    $github_url, 
+                    $folder, 
+                    $reg_file, 
+                    $iDP_config, 
+                    $log_file, 
+                    $event_file,
+                    $version_major, 
+                    $version_minor, 
+                    $version_hotfix, 
+                    $LicenseKey
+                ){
+    
+                        try {
+                                #Set the path  to the server version of Tableau that you want to download
+                                $url = "https://downloads.tableau.com/esdalt/"+$version_major+"."+$version_minor+"."+$version_hotfix+"/TableauServer-64bit-"+$version_major+"-"+$version_minor+"-"+$version_hotfix+".exe"
+
+                                #Create Download folder if it does not exist
+                                (![System.IO.File]::Exists($Folder))
+                                    {
+                                        
+                                        New-Item -Path $Folder -ItemType Directory
+                                    }
+
+                                #Download the server installation file
+                                Write-ToLog -text "Started downloading Tableau Server media" 
+                                Invoke-WebRequest -Uri $url -OutFile $folder"tableau_server.exe"
+                                Write-ToLog -text "Finnished downloading Tableau Server media"
+
+                                #Download reg_file
+                                Write-ToLog -text  "Downloading reg file"
+                                Invoke-WebRequest -Uri $github_url$reg_file -OutFile $folder$reg_file
+                                Write-ToLog -text "Download of regfile completed successfully"
+                                
+                                #Download iDP file
+                                Write-ToLog -text  "Downloading reg file"
+                                Invoke-WebRequest -Uri $github_url + $iDP_config -OutFile $folder$iDP_config
+                                Write-ToLog -text "Download of regfile completed successfully"
+
+                                #Install Switches and Properties for Tableau Server
+                                #https://help.tableau.com/current/server/en-us/silent_installer_flags.htm
+                                #Start silent Tableau server installation
+                                Write-ToLog -text  "Starting Tableau Server installation"
+                                Start-Process -FilePath $folder"tableau_server.exe" -ArgumentList "'/install /silent /ACCEPTEULA = 1 /LOG '+$folder$log_file+'" -Verb RunAs -Wait
+                                Write-ToLog -text  "Tableau server installation finnished"
+
+
+                                #Add TSM to Windows Path
+                                Write-ToLog -text "Adding TSM to local Windows system PATH"
+                                $wd =  'C:\Program Files\Tableau\Tableau Server\packages\'
+                                $tsm_path = Get-ChildItem $wd\bin.$version_major$version_minor.$version_hotfix* -Directory | Select-Object -Property name  
+                                $tsm_path = $wd+$tsm_path.Name
+                                
+                                $Env:path += $tsm_path
+                                #Activate license
+                                Write-Host "Activating Tableau Server License"
+                                if($LicenseKey -eq '')
+                                {
+                                    Write-Host "-LicenseKey parameter was not entered       ! Please enter your license key or type Trial to activeate a 14 day trial key."
+                                }
+                                elseif($LicenseKey.ToLower() -eq "trial")
+                                {
+                                    Write-ToLog -text "Staring Tableau Server Trial activation"
+                                    Invoke-Expression "tsm licenses activate -t"
+                                    Write-ToLog -text "Completed Tableau Server Trial activation"
+                                }
+                                else 
+                                {
+                                    Write-ToLog -text "Staring Tableau Server activation"
+                                    Invoke-Expression "tsm licenses activate -k '$LicenseKey'"
+                                    Write-ToLog -text "Completed Tableau Server activation"
+                                }
+                                
+                                #Register Tableau Server
+                                Write-ToLog -text  "Starting Tableau Server registration"
+                                Invoke-Expression "tsm register --file '$Folder$reg_file'"
+                                Write-ToLog -text "Completed Tableau Server registration"
+
+                                #Set local repository
+                                Write-ToLog -text "Starting Tableau Server local Repository setup"
+                                Invoke-Expression "tsm settings import -f '$Folder$iDP_config'"
+                                Write-ToLog -text "Completed Tableau Server local Repository setup"
+
+                                #Apply pending changes
+                                Write-ToLog -text "Applying pending TSM changes"
+                                Invoke-Expression "tsm pending-changes apply"
+                                Write-ToLog -text  "Completed TM changes"
+
+                                #Initialize configuration
+                                Write-ToLog -text  "Initializing Tableau Server"
+                                Invoke-Expression "tsm initialize"
+                                Write-ToLog -text  "Tableau Server initialized"
+
+                                #Initialize configuration
+                                Write-ToLog -text  "Starting Tableau Server"
+                                Invoke-Expression "tsm start"
+                                Write-ToLog -text  "Tableau Server started"
+                        }
+                        catch
+                            {
+                                Write-ToLog -text $PSItem.Exception.Message
+                            }
+}
 
 
